9ab8382a52378301dd1f4f225f3664515a4a9221
VÍTOR RODRIGUES <vitor_rodrigues3@estudante.sesisenai.org.br>
2024-10-07 17:18:52 -0300

    Primeiro Commit

---
+++ classe.dart
@@ -0,0 +1,45 @@
+class Pessoa {
+  String _nome = '';
+  int _idade = 0;
+
+  void set nome(String novoNome) {
+    if(novoNome.isNotEmpty) {
+      _nome = novoNome;
+    } else{
+      print("Nome não pode ser vazio");
+    }
+  }
+
+  String get nome => _nome;
+
+  void set idade(int novaIdade) {
+    if(novaIdade > 0) {
+      _idade = novaIdade;
+    } else {
+      print("Idade deve ser maior do que zero");
+    }
+  }
+
+  int get idade => _idade;
+}
+
+void main() {
+  Pessoa pessoa = Pessoa();
+
+pessoa.nome = "João";
+pessoa.idade = 30;
+
+  if(pessoa.nome == "" || pessoa.idade <= 0){
+    return null;
+  }else{
+
+  
+  
+  print("Nome: ${pessoa.nome}");
+
+  
+  print("Idade: ${pessoa.idade}");
+
+  }
+
+}
---
+++ classeconta.dart
@@ -0,0 +1,27 @@
+class Conta{
+  double saldo = 1000;
+  double _saque = 200;
+
+  double get saque{
+    if (_saque > saldo) {
+      print("Erro: O valor do saque não pode ser maior que o saldo disponível.");
+      return saldo;
+    }
+    // validação vai aqui
+    return this._saque;
+  }
+  set saque(double saque){
+    if (saque <= saldo) {
+      this._saque = saque;
+    } else {
+      print("Erro: Tentativa de saque maior que o saldo disponível.");
+    }
+  }
+}
+
+void main() {
+  Conta conta = Conta();
+  conta.saque = 2000;
+
+  print("O valor do saque foi de ${conta.saque}");
+}
